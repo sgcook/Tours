02bb3b737af31859d0c58109135584e53ef5bf02
Sara Cook <saracook087@gmail.com>
2022-01-10 20:19:38 -0500

    Added loading bar image

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Loading.css";
+import loadingTime from "./loading-time.jpg";
 
 const Loading = () => {
   return (
     <div className="loading">
       <h1>Loading...</h1>
+      <img src={loadingTime} alt="loading bar half complete" />
     </div>
   );
 };
